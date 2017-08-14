62c2190d4dd4650db7d7f126cb7834b0e9428061
daniL16 <dani16lp@gmail.com>
2017-08-14 12:52:43 +0200

    cambio de apariencia

--- investiga/inc/nav.php
+++ investiga/inc/nav.php
@@ -1,27 +1,26 @@
 <aside id="nav">
     <ul>
-        <li><a href="/~daniellg1617sep/investiga/index.php">Inicio</a></li>
-        <li><a href="/~daniellg1617sep/investiga/miembros.php">Miembros</a></li>
-        <li><a href="/~daniellg1617sep/investiga/publicaciones.php">Publicaciones</a></li>
-        <li><a href="/~daniellg1617sep/investiga/proyectos.php">Proyectos</a></li>
+        <li><a class='enlaceboton' href="/~daniellg1617sep/investiga/index.php">Inicio</a></li>
+        <li><a class='enlaceboton' href="/~daniellg1617sep/investiga/miembros.php">Miembros</a></li>
+        <li><a class='enlaceboton' href="/~daniellg1617sep/investiga/publicaciones.php">Publicaciones</a></li>
+        <li><a class='enlaceboton' href="/~daniellg1617sep/investiga/proyectos.php">Proyectos</a></li>
         <?php
-        //session_start();
         if(isset($_SESSION['usuario']) and $_SESSION['estado'] == 'Autenticado'){
             echo "<li id='add'>Añadir
                   <ul id='submenu_add'>
-                    <li><a href='/~daniellg1617sep/investiga/registrar_publicacion.php'>Añadir publicación</a></li>
-                    <li><a href='/~daniellg1617sep/investiga/registrar_proyecto.php'>Añadir proyecto</a></li></ul></li>";
+                    <li><a class='enlaceboton' href='/~daniellg1617sep/investiga/registrar_publicacion.php'>Añadir publicación</a></li>
+                    <li><a class='enlaceboton' href='/~daniellg1617sep/investiga/registrar_proyecto.php'>Añadir proyecto</a></li></ul></li>";
             if($_SESSION['admin']){
                echo "<li id='admin'>Tareas de administracion
                      <ul id='submenu_admin'>
-                        <li><a href='/~daniellg1617sep/investiga/log.php'>Ver log</a></li>
-                        <li><a href='/~daniellg1617sep/investiga/php/backup.php'>Realizar un backup</a></li>
-                        <li><a href='/~daniellg1617sep/investiga/php/restaurarBD.php'>Restaurar BD</a></li>
+                        <li><a class='enlaceboton' href='/~daniellg1617sep/investiga/log.php'>Ver log</a></li>
+                        <li><a class='enlaceboton' href='/~daniellg1617sep/investiga/php/backup.php'>Realizar un backup</a></li>
+                        <li><a class='enlaceboton' href='/~daniellg1617sep/investiga/php/restaurarBD.php'>Restaurar BD</a></li>
                     </ul></li>";
             }
         }
         ?>
-        <li><a href="/~daniellg1617sep/investiga/doc/documentacion.pdf">Documentación</a></li>
+        <li><a class='enlaceboton' href="/~daniellg1617sep/investiga/doc/documentacion.pdf">Documentación</a></li>
         
     </ul>
 </aside>
